4641be3a219eeff67760f570db96cab8be6d47ca
slunecnicee <129013318+slunecnicee@users.noreply.github.com>
2023-12-17 17:46:02 +0400

    new base api

--- src/servises/baseApi.js
+++ src/servises/baseApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://digitalinstitute-amazon.azurewebsites.net";
+const baseURL = "https://ngglobalwebapi20231210182820.azurewebsites.net";
 
 export const baseAPI = axios.create({
   baseURL,
